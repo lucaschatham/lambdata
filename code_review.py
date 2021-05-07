2bcc0997860c64702732c731143800e850f411f1
Lucas Chatham <chathamworks@gmail.com>
2021-05-07 14:25:54 +1000

    Add files via upload

---
+++ code_review.py
@@ -0,0 +1,29 @@
+import math
+import sys
+
+
+def exampl1():
+    # THIS IS A LONG COMMENT
+    some_tuple = (1, 2, 3, 'a'):
+        some_variable = {"long": 'LONG CODE LINES should',
+                         'other': [math.pi, 100, 200, 300, 9999292929292, "T"],
+                         "more": {"inner": "THIS"},
+                         "data": [444, 5555, 222, 3, 5]}
+    return (some_tuple, some_variable)
+
+
+def example_2():
+    return {"has_key() is deprecated": True}
+
+
+class Example3(object):
+    def __init__(self, bar):
+        if bar bar + = 1
+        bar = bar * bar
+        return bar
+        else:
+            some_string =
+            """
+            INDENTATION IN MULTIPLE STRINGS SHOULD NOT BE TOUCHED
+            """
+        return(sys.path, some_string)
